6a22a80cc8e10bed20ed3ba6f1aa660333558a95
KhadeejaShah-skye <khadeeja.shah@skyelectric.com>
2026-01-26 16:34:33 +0500

    adding old code of server

--- server.js
+++ server.js
@@ -1,63 +1,9 @@
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// let userSocket = null;
-// let nocSocket = null;
-
-// app.use(express.static("public"));
-
-// app.get("/user", (_, res) => res.sendFile(__dirname + "/public/user.html"));
-// app.get("/noc", (_, res) => res.sendFile(__dirname + "/public/noc.html"));
-
-// io.on("connection", socket => {
-//   console.log("Connected:", socket.id);
-
-//   socket.on("join", role => {
-//     if (role === "user") userSocket = socket;
-//     if (role === "noc") nocSocket = socket;
-//   });
-
-//   socket.on("call_noc", () => {
-//     if (nocSocket) {
-//       nocSocket.emit("incoming_call");
-//     }
-//   });
-
-//   socket.on("accept_call", () => {
-//     if (userSocket) userSocket.emit("call_accepted");
-//   });
-
-//   socket.on("signal", ({ to, data }) => {
-//     if (to === "noc" && nocSocket) nocSocket.emit("signal", data);
-//     if (to === "user" && userSocket) userSocket.emit("signal", data);
-//   });
-
-//   socket.on("disconnect", () => {
-//     if (socket === userSocket) userSocket = null;
-//     if (socket === nocSocket) nocSocket = null;
-//   });
-// });
-
-// server.listen(3000, () => console.log("Server running on http://localhost:3000"));
-const fs = require("fs");
-const https = require("https");
 const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
-// Read SSL certificate
-const options = {
-  key: fs.readFileSync("key.pem"),
-  cert: fs.readFileSync("cert.pem"),
-};
-
-const server = https.createServer(options, app);
+const server = http.createServer(app);
 const io = new Server(server);
 
 let userSocket = null;
@@ -68,16 +14,18 @@ app.use(express.static("public"));
 app.get("/user", (_, res) => res.sendFile(__dirname + "/public/user.html"));
 app.get("/noc", (_, res) => res.sendFile(__dirname + "/public/noc.html"));
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   console.log("Connected:", socket.id);
 
-  socket.on("join", (role) => {
+  socket.on("join", role => {
     if (role === "user") userSocket = socket;
     if (role === "noc") nocSocket = socket;
   });
 
   socket.on("call_noc", () => {
-    if (nocSocket) nocSocket.emit("incoming_call");
+    if (nocSocket) {
+      nocSocket.emit("incoming_call");
+    }
   });
 
   socket.on("accept_call", () => {
@@ -95,7 +43,59 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 3000;
-server.listen(PORT, () =>
-  console.log(`Server running on https://192.168.50.192:${PORT}`)
-);
+server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// const fs = require("fs");
+// const https = require("https");
+// const express = require("express");
+// const { Server } = require("socket.io");
+
+// const app = express();
+
+// // Read SSL certificate
+// const options = {
+//   key: fs.readFileSync("key.pem"),
+//   cert: fs.readFileSync("cert.pem"),
+// };
+
+// const server = https.createServer(options, app);
+// const io = new Server(server);
+
+// let userSocket = null;
+// let nocSocket = null;
+
+// app.use(express.static("public"));
+
+// app.get("/user", (_, res) => res.sendFile(__dirname + "/public/user.html"));
+// app.get("/noc", (_, res) => res.sendFile(__dirname + "/public/noc.html"));
+
+// io.on("connection", (socket) => {
+//   console.log("Connected:", socket.id);
+
+//   socket.on("join", (role) => {
+//     if (role === "user") userSocket = socket;
+//     if (role === "noc") nocSocket = socket;
+//   });
+
+//   socket.on("call_noc", () => {
+//     if (nocSocket) nocSocket.emit("incoming_call");
+//   });
+
+//   socket.on("accept_call", () => {
+//     if (userSocket) userSocket.emit("call_accepted");
+//   });
+
+//   socket.on("signal", ({ to, data }) => {
+//     if (to === "noc" && nocSocket) nocSocket.emit("signal", data);
+//     if (to === "user" && userSocket) userSocket.emit("signal", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     if (socket === userSocket) userSocket = null;
+//     if (socket === nocSocket) nocSocket = null;
+//   });
+// });
+
+// const PORT = 3000;
+// server.listen(PORT, () =>
+//   console.log(`Server running on https://192.168.50.192:${PORT}`)
+// );
